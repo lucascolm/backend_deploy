df1d10f20578ce5286e6cfb8dc2299133c9cbd77
lucascolm <lucaspaganelli20@gmail.com>
2022-10-31 14:59:56 -0300

    fix

--- index.js
+++ index.js
@@ -17,12 +17,13 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+const port=process.env.PGPORT||3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(5497, () => {
+  server.listen(port, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
