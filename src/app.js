fb6d52611b21bc5e970551a2c1a83a5dfb6088b1
Lucas colman <lucaspaganelli20@gmail.com>
2024-05-04 16:39:14 -0300

    probando

--- src/app.js
+++ src/app.js
@@ -18,25 +18,11 @@ var corsOptions = {
   methods: "*",
   credentials: true,
 };
-server.use(cors({
-  origin: "https://acreditaciones-mgnt.netlify.app",
-  credentials: true
-}));
+server.use(cors(corsOptions));
 server.use((req, res, next) => {
   console.log('Request Headers:', req.headers);
   next();
 });
-server.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', 'https://acreditaciones-mgnt.netlify.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.status(200).send();
-  } else {
-    next();
-  }
-});
-
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
